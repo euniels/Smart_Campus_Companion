6bb082ab606f153eb9d27f25c59aa48215fe7211
leigcsn-sys <jlgacusana@gmail.com>
2026-01-31 22:28:10 +0800

    feat: implement hardcoded campus department list

--- app/src/main/java/com/example/smart_campus_companion/data/CampusDeptData.kt
+++ app/src/main/java/com/example/smart_campus_companion/data/CampusDeptData.kt
@@ -1,4 +1,38 @@
 package com.example.smart_campus_companion.data
 
-class CampusDeptData {
+import com.example.smart_campus_companion.data.model.Department
+
+object CampusDeptData {
+    val departments = listOf(
+        Department(
+            id = 1,
+            name = "College of Information Technology",
+            location = "Innovation Hall, 2nd Floor",
+            contactEmail = "it.support@campus.edu"
+        ),
+        Department(
+            id = 2,
+            name = "College of Engineering",
+            location = "Main Tech Building",
+            contactEmail = "eng.dean@campus.edu"
+        ),
+        Department(
+            id = 3,
+            name = "College of Arts and Sciences",
+            location = "Old Wing, Room 101",
+            contactEmail = "cas.office@campus.edu"
+        ),
+        Department(
+            id = 4,
+            name = "Student Affairs Office",
+            location = "Student Center, Ground Floor",
+            contactEmail = "sao@campus.edu"
+        ),
+        Department(
+            id = 5,
+            name = "University Library",
+            location = "Central Plaza",
+            contactEmail = "library.help@campus.edu"
+        )
+    )
 }
